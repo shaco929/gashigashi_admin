39082ddc8a88802b440271a94a8841c734fe8b94
shaco <shaco928.gmail.com>
2026-01-24 23:03:09 +0900

    修正

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,30 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "http://127.0.0.1:54321";
-const supabaseKey = "sb_secret_N7UND0UgjKTVK-Uodkm0Hg_xSvEMPvz";
+import dotenv from "dotenv";
 
-export const supabaseAdmin = createClient(supabaseUrl, supabaseKey);
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseSecretKey = process.env.SUPABASE_SECRET_KEY;
+
+export const supabaseAdmin = supabaseSecretKey 
+  ? createClient(supabaseUrl, supabaseSecretKey)
+  : null;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export function createFutureDate(hoursFromNow) {
+  return roundToHour(new Date(Date.now() + hoursFromNow * 60 * 60 * 1000));
+}
+
+// 時刻を0分0秒に丸める
+export function roundToHour(date) {
+  const rounded = new Date(date);
+  rounded.setMinutes(0, 0, 0); // 分、秒、ミリ秒を0に
+  return rounded;
+}
+
+// 現在時刻を0分0秒に丸める
+export function getCurrentHour() {
+  return roundToHour(new Date());
+}
