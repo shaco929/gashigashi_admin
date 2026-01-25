d5bfbf9fda3fa49f2c441da0b7aa6be7b3748118
shaco <shaco928.gmail.com>
2026-01-25 13:48:43 +0900

    権限修正

--- src/supabase.js
+++ src/supabase.js
@@ -1,28 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabaseSecretKey = process.env.SUPABASE_SECRET_KEY;
-
-export const supabaseAdmin = supabaseSecretKey 
-  ? createClient(supabaseUrl, supabaseSecretKey)
-  : null;
+const supabaseKey = process.env.SUPABASE_KEY; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
-
-export function createFutureDate(hoursFromNow) {
-  return roundToHour(new Date(Date.now() + hoursFromNow * 60 * 60 * 1000));
-}
 
 export function roundToHour(date) {
   const rounded = new Date(date);
   rounded.setMinutes(0, 0, 0); 
   return rounded;
-}
-
-export function getCurrentHour() {
-  return roundToHour(new Date());
 }
